5e7820163ebc1c9afa4979bb10f924daeb2f62f2
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2018-02-24 13:00:55 -0600

    Update and rename HW2 to HW2.R

--- HW2.R
+++ HW2.R
@@ -1,95 +1,66 @@
----
-title: 'HW #2'
-author: "Jordan Hoehne"
-date: "9/30/2017"
-output:
-  word_document: default
-  pdf_document: default
----
-##Part 1
+#I manually entered the data for the OU Football Teams Scores for the 2016 Season and 2017 Season. The data came from soonerstats.com and I created vecrtors in the global environment to reference the data. 
 
-1. I manually entered the data for the OU Football Teams Scores for the 2016 Season and 2017 Season. The data came from soonerstats.com and I created vecrtors in the global environment to reference the data. 
-```{r}
 Opponent.Name <- c("Houston", "Louisiana Monroe", "Ohio State", "TCU",
                   "Texas", "Kansas State", "Texas Tech", "Kansas", "Iowa State", "Baylor", "West Virginia",
                   "Oklahoma State", "Auburn", "UTEP", "Ohio State", "Tulane", "Baylor")
 OU.Score <- c(23, 59, 24, 52, 45, 38, 66, 56, 34, 45, 56, 38, 35, 56, 31, 56, 49)
 Opponent.Score <- c(33, 17, 45, 46, 40, 17, 59, 3, 24, 24, 28, 20, 19, 7, 16, 14, 41)
 
-```
 
-2. I use the list command to show all of the data for Opponent.Name. 
+#2. I use the list command to show all of the data for Opponent.Name. 
 
-```{r}
 list(Opponent.Name)
-```
 
-3. In order to see the data that was input into our vectors above, we build a data frame so that we can easily visualize the data. 
+#3. In order to see the data that was input into our vectors above, we build a data frame so that we can easily visualize the data. 
 
-```{r}
 OUdata <- data.frame(Opponent.Name, OU.Score, Opponent.Score)
-```
 
-4. OU.Score[c(2,4)] helps the user reference specific data and is the result of the combine values function. This provides the user the value of the datum element in the Ou.Score referenceing row's 2 and 4.  
+#4. OU.Score[c(2,4)] helps the user reference specific data and is the result of the combine values function. This provides the user the value of the datum element in the Ou.Score referenceing row's 2 and 4.  
 
-```{r}
 OU.Score[c(2,4)]
-```
 
-5. 
+#5. 
 
-```{r}
 OU.Score2 <- OU.Score/10
 list(OU.Score2)
-```
 
-6. 
 
-```{r}
 OU.Score3 <- OU.Score/OU.Score[c(1)]
 list(OU.Score3)
-```
 
-7. The spread of each game is listed below. Negative values indicate a loss and positive values indicate a win.
+#7. The spread of each game is listed below. Negative values indicate a loss and positive values indicate a win.
 
-```{r}
 
 Spread <- (c(OU.Score) - c(Opponent.Score))
 list(Spread)
-```
-8. Using the length() function in R, you are able to count the number of elements used for a given vector. length() is used over count() as count will not list duplicate a value in it's count rather it will increase its frequency associated with the given variable. 
 
-```{r}
+#8. Using the length() function in R, you are able to count the number of elements used for a given vector. length() is used over count() as count will not list duplicate a value in it's count rather it will increase its frequency associated with the given variable. 
+
 length(OU.Score)
-```
 
-9. Total points scored from the beginning of the 2016 season to now.
+#9. Total points scored from the beginning of the 2016 season to now.
 
-```{r}
 sum(OU.Score)
-```
 
-10. The calculation for the average score per game and it's result, the average points per game.
+#10. The calculation for the average score per game and it's result, the average points per game.
 
-```{r}
 sum(OU.Score) / length(OU.Score)
-```
-## Part 2
 
-1. In order to import our csv. files, it is vital that we set our working directory first.
-```{r}
+
+##################Part 2
+
+
+#1. In order to import our csv. files, it is vital that we set our working directory first.
 getwd()
 setwd("/Users/jhoehne/Desktop/Econ 5023/HW/HW2/HW2Q2")
 list.files()
 Kenya <- read.csv("Kenya.csv")
 Sweden <- read.csv("Sweden.csv")
 World <- read.csv("world.csv")
-```
 
-2. For Question 2, I will calculate the CBR or Crude Birth Rate. In order to calculate CBR, the user divides "Total number of births during the period" by "Total number of person-years lived during the period". Through the list() command, the user is able to see the rates for each country for any given time period. Through the analysis, the user can see that the birthrates decline between 1950-1955 and 2005-2010. This could be due to a number of factors as people living longer, strain on resources, a rise in disposible income, and preferences.
+#2. For Question 2, I will calculate the CBR or Crude Birth Rate. In order to calculate CBR, the user divides "Total number of births during the period" by "Total number of person-years lived during the period". Through the list() command, the user is able to see the rates for each country for any given time period. Through the analysis, the user can see that the birthrates decline between 1950-1955 and 2005-2010. This could be due to a number of factors as people living longer, strain on resources, a rise in disposible income, and preferences.
 
 
-```{r}
 Kenya$totalpy1950and1955 <- sum(Kenya$py.men[1:15] + Kenya$py.women[1:15])
 Kenya$totalpy2005and2010 <- sum(Kenya$py.men[16:30] + Kenya$py.women[16:30])
 Kenya$totalbirths1950and1955 <- sum(Kenya$births[1:15])
@@ -115,12 +86,10 @@ CBRW2005thru2010 <- World$totalbirths2005and2010/World$totalpy2005and2010
 
 list(CBRK1950thru1955, CBRK2005thru2010, CBRS1950thru1955, CBRS2005thru2010, CBRW1950thru1955, CBRW2005thru2010)
 
-```
 
-3. Below are the calculations for the Age-Specific Birth Rate. The data suggests that Kenya had an ASFR 4x greater than Sweden in period 1950-1955 and a 3x greater ASFR in the period 2005-2010. Perhaps this is becase 
+#3. Below are the calculations for the Age-Specific Birth Rate. The data suggests that Kenya had an ASFR 4x greater than Sweden in period 1950-1955 and a 3x greater ASFR in the period 2005-2010. Perhaps this is becase 
 
 
-```{r}
 ASFRK1950and1955 <- (sum(Kenya$births[4:10])) / (sum(Kenya$py.women[4:10]))
 ASFRK2005and2010 <- (sum(Kenya$births[19:25])) / (sum(Kenya$py.women[19:25]))
 ASFRS1950and1955 <- (sum(Sweden$births[4:10])) / (sum(Sweden$py.women[4:10]))
@@ -130,14 +99,12 @@ ASFRW2005and2010 <- (sum(World$births[19:25])) / (sum(World$py.women[19:25]))
 
 ASFR = data.frame(ASFRK1950and1955, ASFRK2005and2010, ASFRS1950and1955, ASFRS2005and2010, ASFRW1950and1955, ASFRW2005and2010)
 
-```
 
 
-4. We calculate total fertility rate as the age-specific births for a given age range divided by the age-specific person years then multiplying by the number of years in that period. Thus, we see from the formulae below that our Total Fertility Rate designated in the format "TFR.first letter of each country.period of interest".
+#4. We calculate total fertility rate as the age-specific births for a given age range divided by the age-specific person years then multiplying by the number of years in that period. Thus, we see from the formulae below that our Total Fertility Rate designated in the format "TFR.first letter of each country.period of interest".
 
 The data suggests that women are living longer and that there are more births occuring in 2000 since 1950. []
 
-```{r}
 TFR.K.1950and1955 <- sum(5*(sum(Kenya$births[4])) / (sum(Kenya$py.women[4])) + 
   (5*(sum(Kenya$births[5])) / (sum(Kenya$py.women[5])))
 + (5*(sum(Kenya$births[6])) / (sum(Kenya$py.women[6])))
@@ -189,7 +156,6 @@ TFR.W.2005and2010 <- sum(5*(sum(World$births[19])) / (sum(World$py.women[19])) +
 TFR <- data.frame(TFR.K.1950and1955, TFR.K.2005and2010, TFR.S.1950and1955, TFR.S.2005and2010, TFR.W.1950and1955, TFR.W.2005and2010)
 
 
-```
 
 
 
